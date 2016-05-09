66314e80a93484cd948d86f02be66557fcb50c28
Josh Cox <josh@webhosting.coop>
2016-05-09 14:26:56 -0500

    known_hosts

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 cp -a /root/keys/id* /root/.ssh/
+cp -a /root/keys/known_hosts /root/.ssh/
 
 chmod 600 /root/.ssh/id*
+chmod 600 /root/.ssh/known_hosts
 
 chown root. /root/.ssh/id*
+chown root. /root/.ssh/known_hosts
 
 /bin/bash /command.sh
 
